2d5dadb3519e7400ca708654a37964f03bf8e16b
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2022-09-23 15:27:33 -0400

    add error code checks on few unit tests

--- ingest/ledger_transaction_test.go
+++ ingest/ledger_transaction_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stellar/go/xdr"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestChangeAccountChangedExceptSignersInvalidType(t *testing.T) {
@@ -13,7 +14,9 @@ func TestChangeAccountChangedExceptSignersInvalidType(t *testing.T) {
 	}
 
 	assert.Panics(t, func() {
-		change.AccountChangedExceptSigners()
+		_, err := change.AccountChangedExceptSigners()
+		// the following is here only to avoid false-positive warning by the linter.
+		require.NoError(t, err)
 	})
 }
 
