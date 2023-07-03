8e85b8e547a39e1feacf0ba5dacf4bfac3ccb478
Paul Bellamy <paul@stellar.org>
2023-07-03 15:06:21 +0100

    LedgerChangeReader: Support State Expiration & Eviction (#4941)
    * Simplify ScError.Equals
    
    * Add missing case for ScVal.Equals
    
    * Add LedgerEntry.SetContractData and LedgerEntry.SetContractCode methods
    
    * update generated xdr to 0f5e556
    
    * Add LedgerCloseMetaV2 support
    
    * Update ledgerTransaction.GetOperationEvents
    
    * Make LedgerChangeReader emit evictions
    
    * Fixing up after merge
    
    * Add test for LedgerChangeReader extensions and evictions
    
    * Fix typo
    
    * Add xdr.LedgerEntryData.ExpirationLedgerSeq helper
    
    * review feedback

--- xdr/ledger_close_meta.go
+++ xdr/ledger_close_meta.go
@@ -52,20 +52,14 @@ func (l LedgerCloseMeta) TransactionEnvelopes() []TransactionEnvelope {
 	switch l.V {
 	case 0:
 		return l.MustV0().TxSet.Txs
-	case 1:
+	case 1, 2:
 		var envelopes = make([]TransactionEnvelope, 0, l.CountTransactions())
 		var phases []TransactionPhase
-		phases = l.MustV1().TxSet.V1TxSet.Phases
-		for _, phase := range phases {
-			for _, component := range *phase.V0Components {
-				envelopes = append(envelopes, component.TxsMaybeDiscountedFee.Txs...)
-			}
+		if l.V == 1 {
+			phases = l.MustV1().TxSet.V1TxSet.Phases
+		} else {
+			phases = l.MustV2().TxSet.V1TxSet.Phases
 		}
-		return envelopes
-	case 2:
-		var envelopes = make([]TransactionEnvelope, 0, l.CountTransactions())
-		var phases []TransactionPhase
-		phases = l.MustV2().TxSet.V1TxSet.Phases
 		for _, phase := range phases {
 			for _, component := range *phase.V0Components {
 				envelopes = append(envelopes, component.TxsMaybeDiscountedFee.Txs...)
@@ -127,6 +121,9 @@ func (l LedgerCloseMeta) TxApplyProcessing(i int) TransactionMeta {
 	case 1:
 		return l.MustV1().TxProcessing[i].TxApplyProcessing
 	case 2:
+		if l.MustV2().TxProcessing[i].TxApplyProcessing.V != 3 {
+			panic("TransactionResult unavailable because LedgerCloseMeta.V = 2 and TransactionMeta.V != 3")
+		}
 		return l.MustV2().TxProcessing[i].TxApplyProcessing
 	default:
 		panic(fmt.Sprintf("Unsupported LedgerCloseMeta.V: %d", l.V))
@@ -142,6 +139,26 @@ func (l LedgerCloseMeta) UpgradesProcessing() []UpgradeEntryMeta {
 		return l.MustV1().UpgradesProcessing
 	case 2:
 		return l.MustV2().UpgradesProcessing
+	default:
+		panic(fmt.Sprintf("Unsupported LedgerCloseMeta.V: %d", l.V))
+	}
+}
+
+// EvictedLedgerKeys returns the LedgerKeys for both the
+// EvictedTemporaryLedgerKeys and and the EvictedPersistentLedgerEntries in a
+// ledger.
+func (l LedgerCloseMeta) EvictedLedgerKeys() []LedgerKey {
+	switch l.V {
+	case 0, 1:
+		return nil
+	case 2:
+		v2 := l.MustV2()
+		keys := make([]LedgerKey, 0, len(v2.EvictedTemporaryLedgerKeys)+len(v2.EvictedPersistentLedgerEntries))
+		keys = append(keys, l.MustV2().EvictedTemporaryLedgerKeys...)
+		for _, entry := range l.MustV2().EvictedPersistentLedgerEntries {
+			keys = append(keys, entry.LedgerKey())
+		}
+		return keys
 	default:
 		panic(fmt.Sprintf("Unsupported LedgerCloseMeta.V: %d", l.V))
 	}
